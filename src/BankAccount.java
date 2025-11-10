bde35239d20453ea9c461bc19c20a246b93474ef
salahudinileye-cloud <salahudinileye@gmail.com>
2025-11-10 10:24:57 +0100

    Jeg lavede en simpel bank account klasse med nogle metoder for deposit, withdraw og balance.

---
+++ src/BankAccount.java
@@ -0,0 +1,22 @@
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+    public void deposit(double amount) {
+        setBalance(balance + amount);
+    }
+    public void withdraw(double amount) {
+        setBalance(balance - amount);
+
+    }
+    public double getBalance() {
+        return balance;
+    }
+    private void setBalance(double amount) {
+        if (amount > 0) {
+            balance = amount;
+        }
+    }
+}
